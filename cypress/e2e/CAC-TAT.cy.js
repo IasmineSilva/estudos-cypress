e71c91a6799b6d7122fd67f7cd7e565c109cba93
iasmine silva <iasmine948@gmail.com>
2024-07-26 14:05:42 -0300

    procurando o gatinho laranja

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -164,6 +164,10 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             expect(body).to.include('CAC TAT')
         })
     })
-   
+   it('Encontra o gato escondido', function(){
+    cy.get('#cat')
+    .invoke('show')
+    .should('be.visible')
+   })
 })
 
