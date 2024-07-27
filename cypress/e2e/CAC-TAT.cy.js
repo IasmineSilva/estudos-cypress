6132f7ba351346f3257041d273a297b0df9502b6
Iasmine <Iasmine948@gmail.com>
2024-07-27 19:20:49 -0300

    finalizando cypress básico

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -168,6 +168,10 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#cat')
     .invoke('show')
     .should('be.visible')
+    cy.get('#title')
+    .invoke('text', 'CAT TAT')
+    cy.get('#subtitle')
+    .invoke('text', 'Eu 💜 Gatos')
    })
 })
 
