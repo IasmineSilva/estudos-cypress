2f4fa25a47a4559b52d4bb28f99e5f11e3b5f7f9
Iasmine <Iasmine948@gmail.com>
2026-02-07 16:31:58 -0300

    hardcoded-assertion

--- boas-praticas-em-automacao-de-testes-com-cypress-v2/cypress/e2e/testes-instáveis/amostra.cy.js
+++ boas-praticas-em-automacao-de-testes-com-cypress-v2/cypress/e2e/testes-instáveis/amostra.cy.js
@@ -1,13 +1,14 @@
 import { faker } from '@faker-js/faker'
 
-describe('Prática ruim de testes instáveis', () => {
+describe('Prática ruim de testes instáveis - Exercício', () => {
   beforeEach(() => {
-    cy.visit('https://wlsf82-hacker-stories.web.app')
+    cy.intercept(
+      'GET',
+      '**/search**'
+    ).as('getStories')
 
-    cy.contains('p', 'Carregando ...')
-      .should('be.visible')
-    cy.contains('p', 'Carregando ...')
-      .should('not.exist')
+    cy.visit('https://wlsf82-hacker-stories.web.app')
+    cy.wait('@getStories')
   })
 
   Cypress._.times(10, () => {
@@ -15,11 +16,55 @@ describe('Prática ruim de testes instáveis', () => {
       Cypress._.times(6, () => {
         cy.buscar(faker.random.word())
       })
+      cy.wait('@getStories')
 
-      cy.contains('p', 'Carregando ...')
-        .should('be.visible')
-      cy.contains('p', 'Carregando ...')
-        .should('not.exist')
+      cy.get('.last-searches button')
+        .should('have.length', 5)
+    })
+  })
+})
+
+describe('Prática ruim de testes instáveis - Exercício extra 1', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
+    cy.visit('https://wlsf82-hacker-stories.web.app')
+    cy.wait('@getStories')
+  })
+
+  Cypress._.times(10, () => {
+    it('mostra um máximo de 5 botões para os últimos termos pesquisados', () => {
+      Cypress._.times(6, () => {
+        cy.buscar(faker.random.word())
+      })
+      cy.wait('@getStories')
+
+      cy.get('.last-searches button')
+        .should('have.length', 5)
+    })
+  })
+})
+
+describe('Prática ruim de testes instáveis - Exercício extra 2', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    )
+
+    cy.visit('https://wlsf82-hacker-stories.web.app')
+  })
+
+  Cypress._.times(10, () => {
+    it('mostra um máximo de 5 botões para os últimos termos pesquisados', () => {
+      Cypress._.times(6, () => {
+        cy.buscar(faker.random.word())
+      })
 
       cy.get('.last-searches button')
         .should('have.length', 5)
